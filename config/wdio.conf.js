e726dde329627cd63701ee17e239438865836abb
metroprog <h-e-l-e-n@i.ua>
2022-10-20 14:20:10 +0300

    gh actions

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -132,7 +132,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: [['allure', {outputDir: 'allure-results'}]],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+    }]],
 
 
     
